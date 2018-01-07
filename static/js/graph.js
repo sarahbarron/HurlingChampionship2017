28081933de38d52fe811908b3285f56b5380a621
Sarah Barron <sarahflavin@yahoo.com>
2018-01-07 18:50:19 +0000

    teams chart

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,31 +8,59 @@ function makeGraphs(error, HurlingStats) {
         throw error;
     }
 
+    HurlingStats.forEach(function (d) {
+       d["total_goals"] = +d["total_goals"];
+       d["total_points"] = +d["total_points"];
+    });
+
 
     //Create a CROSSFILETER instance
     var ndx = crossfilter(HurlingStats);
 
-    //dimensions
+
+    //  DIMENSIONS
+    //stage of all ireland
     var stageOfAllIreland = ndx.dimension(function (d) {
         return d['stage_of_all_ireland'];
     });
 
+    //teams
+    var teams = ndx.dimension(function (d) {
+        return d['teams'];
+    });
+
 
     //grouping data & metrics
-    var numStageOfAllIreland = stageOfAllIreland.group() ;
+    var numStageOfAllIreland = stageOfAllIreland.group();
+    var totalGoalsGroup = teams.group().reduceSum(function (d) {
+        return d["total_goals"];
+    }) ;
 
 
     //Charts
+
+    //stage of all ireland
     var stageOfAllIrelandPieChart = dc.pieChart("#stage-of-all-ireland");
+    var teamsChart = dc.barChart("#teams-chart");
+
 
     stageOfAllIrelandPieChart
         .ordinalColors(["#79CED7", "#C96A23", "#D3D1C5", "#F5821F", "#006FC1","#EC2322","#8BC934"])
-        .height(220)
-        .radius(90)
+        .height(400)
+        .radius(100)
         .transitionDuration(1500)
         .dimension(stageOfAllIreland)
         .group(numStageOfAllIreland);
 
+
+    //teams
+    teamsChart
+        .width(500)
+        .height(200)
+        .dimension(teams)
+        .group(totalGoalsGroup)
+        .xAxisLabel("2017 Championship Hurling Teams")
+        .yAxisLabel("Each teams - Goals and Points");
 
     dc.renderAll();
 }
